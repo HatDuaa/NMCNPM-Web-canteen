897f669a2e49b32743fd11daabcd289db04998ad
HatDuaa <loocnguyen123@gmail.com>
2022-12-01 15:26:49 +0700

    thêm loại món ăn

--- home/models.py
+++ home/models.py
@@ -4,10 +4,12 @@ from django.db import models
 class Dish(models.Model):
     name = models.CharField(max_length=200)
     #body = models.TextField()
+    type = models.CharField(max_length=200, null=True)
     remaining = models.PositiveSmallIntegerField()
     date = models.DateTimeField(auto_now_add=False)
     price = models.DecimalField(max_digits=6, decimal_places=1)
     image = models.ImageField(null=True)
+
 
     def _str_(self):
         return self.name
--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import Dish
 from .form import RegistrationForm
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 # def index(request):
@@ -14,6 +15,10 @@ from .form import RegistrationForm
 def list(request):
     data = {'Dishs': Dish.objects.all().order_by('name')}
     return render(request, "pages/home.html", data)
+class DishListView(ListView):
+    queryset = Dish.objects.all().order_by('name')
+    template_name = 'pages/test.html'
+    context_object_name = 'Dishs'
 
 def register(request):
     form = RegistrationForm()
